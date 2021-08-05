5f34187707ca6afa88ace201e7d760c5f136f729
elodiebhs <elodiebouthors@hotmail.com>
2021-08-05 00:51:08 +0000

    hide the contact info

--- contact.js
+++ contact.js
@@ -27,17 +27,21 @@ class Contact extends React.Component {
         <input type="password" placeholder="Password" />
         <input type="submit" />
       </form>)
+
+    const contactInfo = (
+      <ul>
+        <li>
+          client@example.com
+           </li>
+        <li>
+          555.555.5555
+           </li>
+      </ul>
+    );
     return (
       <div id="authorization">
         <h1>{this.state.authorized ? "Contact" : "Enter the Password"}</h1>
-        <ul>
-          <li>
-            client@example.com
-          </li>
-          <li>
-            555.555.5555
-          </li>
-        </ul>
+        
       </div>
     );
   }
